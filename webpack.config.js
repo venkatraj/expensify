84a44a11b2ff1d4a167b43315376e3654849e44f
Venkat <venkat@codinggeek.com>
2020-05-16 13:10:23 +0530

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+};
