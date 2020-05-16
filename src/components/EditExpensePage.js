3aa5472365f77a4fc9086587898e8053eeb97889
Venkat <venkat@codinggeek.com>
2020-05-16 16:33:20 +0530

    using url parameters with react router

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    <h1>EditExpensePage</h1>
-  </div>
-);
+const EditExpensePage = (props) => {
+  const { id } = props.match.params;
+  return (
+    <div>
+      <h1>EditExpensePage</h1>
+      <p>Editing expense with the id of {id}</p>
+    </div>
+  );
+};
 
 export default EditExpensePage;
