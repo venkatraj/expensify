5c2fa2aa9f49a46195eb4d06228f6bf1808b7fd5
Venkat <venkat@codinggeek.com>
2020-05-17 18:00:28 +0530

    combineReducer concept

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,49 @@
 import { createStore, combineReducers } from 'redux';
 
+// Actions
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+const expenseReducerDefaultState = [];
+
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filter Reducer
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: null,
+  endDate: null,
+};
+
+const filterReducer = (state = filterReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters: filterReducer,
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [
     {
