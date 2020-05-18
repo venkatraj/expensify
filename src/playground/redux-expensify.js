8351ee2a0d6eac95ed06ed99b340f6a022fc174d
Venkat <venkat@codinggeek.com>
2020-05-18 10:55:41 +0530

    expense generators

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,37 @@
 import { createStore, combineReducers } from 'redux';
+import { v4 as uuid } from 'uuid';
 
 // Actions
 // ADD_EXPENSE
+const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0,
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt,
+  },
+});
 // REMOVE_EXPENSE
+const removeExpense = ({ id }) => ({
+  type: 'REMOVE_EXPENSE',
+  id,
+});
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates,
+});
+
 // SET_TEXT_FILTER
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -15,6 +42,20 @@ const expenseReducerDefaultState = [];
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        }
+        return expense;
+      });
     default:
       return state;
   }
@@ -42,7 +83,23 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => console.log(store.getState()));
+
+const expenseOne = store.dispatch(
+  addExpense({ description: 'Rent', amount: 400000 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: 'Coffee', amount: 800 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(
+  editExpense(expenseTwo.expense.id, {
+    description: 'Iced Coffee',
+    amount: 850,
+  })
+);
 
 const demoState = {
   expenses: [
