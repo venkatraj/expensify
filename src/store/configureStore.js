e17cb8340ad9e3d88703feda4fe89af88fe7c9e9
Venkat <venkat@codinggeek.com>
2020-05-20 08:52:26 +0530

    enable redux devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,12 +3,15 @@ import expenseReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
 
 const configureStore = () => {
+  /* eslint-disable no-underscore-dangle */
   const store = createStore(
     combineReducers({
       expenses: expenseReducer,
       filters: filterReducer,
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+  /* eslint-enable */
   return store;
 };
 
