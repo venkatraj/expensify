dd0fa520f3fb2d1c505ff9002d825576f30f7d67
Venkat <venkat@codinggeek.com>
2020-05-24 08:46:29 +0530

    async await and babel setup

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -18,3 +18,23 @@ promise
   .catch((error) => {
     console.log('Rejected?', error);
   });
+
+(async () => {
+  try {
+    const result = await new Promise((resolve, reject) => {
+      const random = Math.floor(Math.random() * 10);
+      console.log(random);
+      if (random % 2 === 0) {
+        setTimeout(() => {
+          resolve('Awaited: This is my resolved data!');
+        }, 3000);
+      }
+      setTimeout(() => {
+        reject(Error('Promise rejected due to odd values!'));
+      }, 3000);
+    });
+    console.log(result);
+  } catch (e) {
+    console.log('Rejected Promise', e);
+  }
+})();
